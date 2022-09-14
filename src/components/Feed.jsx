2cfeaddc87596450637c5faf69abab878f53280a
Ayush yadav <20ayush07yadav03@gmail.com>
2022-09-15 00:20:06 +0530

    navbar done

---
+++ src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx:
+    "column", md: "row" } }}>
+      
+    </Stack>
+  )
+}
+
+export default Feed
