1d0cccc100f67397c2483721e507f1e95d693a08
Ayush yadav <20ayush07yadav03@gmail.com>
2022-09-19 23:50:56 +0530

    feed done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const Sidebar = ( { selectedCategory, setSelectedCategory})
- =>  (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) =>  (
     <Stack direction="row"
         sx={{
             overflowY: 'auto',
             height: { sx: 'auto', md: '95%' },
             flexDirection: { md: 'column' },
-
+                                                                                                                        
         }}>
             { categories.map((category) => (
                 <button className="category-btn"
+                onClick={() => setSelectedCategory
+                (category.name)}
                 style = {{
                     background: category.name ===
                     selectedCategory && '#FC1503',
@@ -37,3 +38,5 @@ const Sidebar = ( { selectedCategory, setSelectedCategory})
 
 
 export default Sidebar
+
+
